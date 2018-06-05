9c6e1bc1343d1151fff96e8aaec9b21a164c677c
Dondorito <38208232+Dondorito@users.noreply.github.com>
2018-06-05 18:38:16 +0200

    2

--- LandingPageViaje/javascript.js
+++ LandingPageViaje/javascript.js
@@ -18,16 +18,26 @@ function travel() {
     resultFlights *= 0.9;
   }
 
-  if(yes.checked = true){
+  if (yes.checked = true) {
     var resultCar = nights * 40;
   }
   var result = resultNights + resultFlights + resultCar;
 
-  if(nights >= 3){
+  if (nights >= 3) {
     result -= 20;
-  }
-  else if(nights >= 7){
+  } else if (nights >= 7) {
     result -= 50;
   }
-  alert(result);
+  document.getElementById('textResult').innerHTML = result;
+}
+
+function NintendoSwitch() {
+  document.getElementById('main').style.marginLeft = '-9999px';
+  document.getElementById('travel').style.opacity = '1';
+  document.getElementById('container').style.backgroundImage = 'url("images/city2.jpg")';
+}
+
+function back(){
+  document.getElementById('main').style.marginLeft = '0px';
+  document.getElementById('travek').style.opacity = '0';
 }
